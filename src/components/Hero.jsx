698cbf876ae21a8bde5ab8a256b5fafa2ef7fda9
Taniyodi <104867521+Taniyodi@users.noreply.github.com>
2025-05-19 13:03:23 +0000

    Hero section implementation

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+
+const Hero = () => {
+  return (
+    <div className="relative z-20 flex h-screen flex-col items-center justify-center text-center px-6">
+      <h1 className="text-5xl md:text-6xl font-extrabold text-blue-900">
+        Your Health, Simplified
+      </h1>
+      <p className="mt-4 text-lg md:text-xl font-medium text-black">
+        Book appointments with trusted doctors online or <br />
+        explore expert health advice.
+      </p>
+      <div className="mt-8 flex flex-wrap gap-6 justify-center">
+        <button className="bg-[#B4190B] text-white px-8 py-4 rounded-full text-xl font-semibold hover:bg-red-800">
+          Book Appointment
+        </button>
+        <button className="border-2 border-[#B4190B] text-[#B4190B] px-8 py-4 rounded-full text-xl font-semibold hover:bg-red-700 hover:text-white transition">
+          Xplore Health Tips
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import HeroImage from "../assets/images/hero.jpg"; 
+import Hero from "../components/Hero";
 
 const HomePage = () => {
   return (
-   <div className="relative w-full bg-blue overflow-hidden font-sans">
+    <div className="relative w-full bg-blue overflow-hidden font-sans">
       <div className="absolute inset-0 z-0">
         <img
-          src="/path-to-your-image.jpg"
+          src={HeroImage}
           alt="Doctors"
           className="w-full h-full object-cover"
         />
       </div>
 
-      <div className="absolute inset-0 bg-white bg-opacity-30 z-10" />
+      
+     <div className="absolute inset-0 bg-white bg-opacity-30 z-10 mix-blend-overlay" />
       <Header />
-       <main className="relative z-20 flex flex-col justify-center h-full">
+      <main className="relative z-20 flex flex-col justify-center h-full">
+        <Hero />
         <Footer />
       </main>
-      </div>
+    </div>
   );
 };
 
