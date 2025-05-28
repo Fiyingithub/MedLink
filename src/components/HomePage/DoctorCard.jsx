c4b0b1984abd6950de607450f50839491bde020c
Taniyodi <104867521+Taniyodi@users.noreply.github.com>
2025-05-28 06:30:09 +0000

    few fixes

--- src/components/HomePage/DoctorCard.jsx
+++ src/components/HomePage/DoctorCard.jsx
@@ -1,7 +1,7 @@
 const DoctorCard = ({ name, specialty, image }) => {
   return (
     <div className="bg-[#5962B6] rounded-lg overflow-hidden shadow-md text-center">
-      <img src={image} alt={name} className="w-full h-64 object-cover" />
+     <img src={image} alt={name} className="w-full h-64 object-cover object-top" />
       <div className="bg-indigo-600 text-white p-4">
         <h3 className="font-semibold text-lg">{name}</h3>
         <p className="text-sm mb-2">{specialty}</p>
